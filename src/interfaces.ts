7a9cee5586ac25911479231d0115bf0f3c9df28a
enzof10 <enzo_17_05@hotmail.com>
2023-01-05 19:50:16 -0300

    add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+interface Book {
+    id?: number;
+    title? : string;
+    author?: string;
+}
+
+const book:Book = {
+    id: 1,
+    title: "primero",
+};
+
+let books: Array<Book> = []
+
+books.push({
+    id: 1212
+})
+
---
+++ src/interfaces_extends_implements.ts
@@ -0,0 +1,38 @@
+interface Person {
+    id: number;
+    name: string;
+}
+
+interface Employee extends Person {
+    dpto? : string
+}
+
+interface Customer extends Person{
+    country : string
+}
+
+
+const employee : Employee = {
+    id :1,
+    name: "uno"
+}
+
+const customer : Customer = {
+    id :1,
+    name: "uno",
+    country :"uuna country"
+}
+
+
+interface Animal {
+    name: string
+    getDogs: () => void
+    getCats?: () => void
+}
+
+class Zoo implements Animal {
+    name = "Muhh";
+    getDogs (): void{
+        // logica aca
+    };
+}
